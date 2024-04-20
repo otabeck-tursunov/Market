9d1f3a2d146f5041cc155b05fd27c1eaaa425bb2
Otabek Tursunov <otabecktursunov@gmail.com>
2024-04-20 08:29:10 +0500

    commit

--- Market/settings.py
+++ Market/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = str(env('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
